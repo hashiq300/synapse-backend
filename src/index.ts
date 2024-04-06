245f9a50b790d4b676fa3497318138a2a381ab2f
hashiq300 <mohdhashique10@gmail.com>
2024-04-06 13:25:14 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(cors());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
